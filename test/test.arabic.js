de21ea3f5f5bec794b56a1135139ae10f1977697
Ali Sadattalab <xbox3000@gmail.com>
2013-01-31 02:14:22 +0330

    revert my heavy changes to remote old head

--- test/test.arabic.js
+++ test/test.arabic.js
@@ -4,11 +4,11 @@ var assert = require('assert');
 
 describe('convert', function () {
     it('should change Arabic characters to Persian', function (done) {
-        assert.equal("علی", persianJs("علي").toPersian());
+        assert.equal("علی", persianJs("علي").toPersianChar());
         done();
     });
     it('should change Arabic numbers to Persian', function (done) {
-        assert.equal("۳۴۵", "٣٤٥".toPersian());
+        assert.equal("۳۴۵", persianJs("٣٤٥").toPersianNumber());
         done();
     });
 });
